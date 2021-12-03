b53315e8333851771acfd6bf1032191d169d9251
AdinaFakih <addy.fakih@gmail.com>
2021-12-03 14:02:42 +0500

    I made some changes, and now I'm sad

--- src/main/java/twelveBase/TestBase.java
+++ src/main/java/twelveBase/TestBase.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.message.LoggerNameAwareMessage;
+//import org.apache.logging.log4j.message.LoggerNameAwareMessage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
